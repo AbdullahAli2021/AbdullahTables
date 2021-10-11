fb46942408975273834574c5f4d8ae554172a644
Abdulah Ali <w0782917@myscc.ca>
2021-10-11 17:34:12 -0400

    Add project files.

---
+++ AbdullahTables/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AbdullahTables.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AbdullahTables/Readme.md
@@ -0,0 +1,2 @@
+﻿Abdullah tables Project:
+
